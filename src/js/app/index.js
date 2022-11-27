c40fc9267321a02e59fd155c751860f451bd4bff
Andre Arko <andre@arko.net>
2022-11-26 23:42:51 -0800

    re-indent the handlebars and css

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -4,7 +4,7 @@ import "../../css/app.css"
 if (ENV === 'development') {
   const script = document.createElement('script');
   script.src = `http://${
-    (location.host || 'localhost').split(':')[0]
+  (location.host || 'localhost').split(':')[0]
   }:35729/livereload.js?snipver=1`;
   document.head.append(script);
   console.log('Reload script added');
