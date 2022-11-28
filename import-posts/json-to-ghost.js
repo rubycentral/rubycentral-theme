9a691e74acb3ae326cc70ec6ed60012201c0e5b0
Andre Arko <andre@arko.net>
2022-11-27 18:53:21 -0800

    mobiledoc has to be a string

--- import-posts/json-to-ghost.js
+++ import-posts/json-to-ghost.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const json = JSON.parse(fs.readFileSync('posts.json', 'utf8'));
 json.data.posts.forEach((post) => {
-  post.mobiledoc = converter.toMobiledoc(post.html)
+  post.mobiledoc = JSON.stringify(converter.toMobiledoc(post.html))
 });
 
 fs.writeFileSync('ghost-posts.json', JSON.stringify(json), 'utf8');
