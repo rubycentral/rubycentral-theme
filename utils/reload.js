a99bfe9ca7ab0d4e03a36baf241138e223964f27
Andre Arko <andre@arko.net>
2022-12-11 17:46:02 -0800

    switch from livereload to browser-sync

--- utils/reload.js
+++ utils/reload.js
@@ -1,5 +1,9 @@
-const livereload = require('livereload');
-
-const server = livereload.createServer();
-server.watch('../assets');
-console.log('Reload server started...');
+const bs = require("browser-sync").create()
+bs.watch("*.hbs").on("change", bs.reload)
+bs.watch("assets/dist/*.js").on("change", bs.reload)
+bs.watch("assets/dist/*.css").on("change", function() { bs.reload("*.css") })
+bs.init({
+  proxy: "http://localhost:2368",
+  port: 2369,
+  ui: { port: 2370 }
+})
