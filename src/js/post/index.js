fd7ab3f9640734889dd15e1f6d521a1b79adc56f
Andre Arko <andre@arko.net>
2024-03-22 14:37:50 -0700

    add tocbot tables of contents to posts

--- src/js/post/index.js
+++ src/js/post/index.js
@@ -1,1 +1,13 @@
 // Ship JS only active on post pages for better performance
+import tocbot from 'tocbot';
+
+tocbot.init({
+    // Where to render the table of contents.
+    tocSelector: '.gh-toc',
+    // Where to grab the headings to build the table of contents.
+    contentSelector: '.post',
+    // Which headings to grab inside of the contentSelector element.
+    headingSelector: 'h2, h3, h4',
+    // Ensure correct positioning
+    hasInnerContainers: true,
+});
