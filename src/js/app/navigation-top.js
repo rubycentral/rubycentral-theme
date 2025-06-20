17280d45bb9215aeebf99bfd6bf93df0185343f4
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2025-06-20 13:25:02 -0400

    feat(top-nav): add mobile dropdown and styles

--- src/js/app/navigation-top.js
+++ src/js/app/navigation-top.js
@@ -1,10 +1,19 @@
 const mobileTrigger = document.getElementById(
     'navigation__mobile-menu-trigger',
 );
+const list = document.querySelector('.navigation--top .navigation__list');
 
 function toggleMobileMenu() {
     this.classList.toggle('closed');
     this.classList.toggle('opened');
+
+    if (this.classList.contains('closed')) {
+        list.classList.add('navigation__list--mobile-closed');
+        list.classList.remove('navigation__list--mobile-opened');
+    } else {
+        list.classList.remove('navigation__list--mobile-closed');
+        list.classList.add('navigation__list--mobile-opened');
+    }
 }
 
 mobileTrigger.addEventListener('click', toggleMobileMenu);
